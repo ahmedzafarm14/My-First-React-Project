49e8bb2441ce8ded796cafc67aab22b96cf39a3a
Ahmad Zafar <ahmedzafarm14@gmail.com>
2023-12-05 19:16:43 +0500

    Completed and removed errors

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Header from './Components/Header'
+import MainSection from './Components/MainSection'
+
+const App = () => {
+
+  return (
+    <div>
+      <Header/>
+      <MainSection/>
+    </div>
+  )
+}
+
+export default App;
